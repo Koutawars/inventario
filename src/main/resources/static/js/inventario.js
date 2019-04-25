a866f05e2ed9a429c6fcd59b6e78993817edf260
Koutawars <admikouta@gmail.com>
2019-04-25 17:04:59 -0500

    todo de inventario

--- src/main/resources/static/js/inventario.js
+++ src/main/resources/static/js/inventario.js
@@ -1,1 +1,122 @@
-console.log("inventario..");
+var editando;
+$(document).ready(function(){
+	$('.modal').modal();
+	$.ajax({
+		method: "GET",
+		url: "/producto",
+		datatype: "JSON",
+		success: function(data, status, jqXHR ){
+			data.forEach(function(i){
+				agregarTabla(i.id, i.nombre, i.precio, i.cantidad);
+			});
+		},
+		error: function(jqXHR , status, e){
+			M.toast({html: 'Error al traer producto... status: '+jqXHR.status});
+			console.log(jqXHR);
+		}
+	});
+	
+	 $('#agregar').click(function(e){
+		  e.preventDefault();
+		  let nombre = $('#nombre').val();
+		  let precio = $('#precio').val();
+		  let cantidad = $('#cantidad').val();
+		  if(nombre != '' && precio != '' && cantidad != ''){
+			  M.toast({html: 'Agregando...'});
+			  // ajax request
+			  
+			  let producto = {
+						nombre,
+						precio,
+						cantidad
+					};
+			  
+				$.ajax({
+					method: "POST",
+					url: "producto/add",
+					datatype: "JSON",
+					contentType: "application/json",
+					data: JSON.stringify(producto),
+					success: function(data, status, jqXHR ){
+						M.toast({html: '¡Inscrito!'});
+						agregarTabla(data.id, data.nombre, data.precio, data.cantidad);
+					},
+					error: function(jqXHR , status, e){
+						M.toast({html: 'Error a crear un producto, status: '+jqXHR.status});
+						console.log(jqXHR);
+					}
+				});
+		  }
+		  
+	  });
+	  $('#editar').click(function(){
+		  let nombre = $('#nombreEdit').val();
+		  let precio = $('#precioEdit').val();
+		  let cantidad = $('#cantidadEdit').val();
+		  if(nombre != '' && precio != '' && cantidad != ''){
+			  // Ajax request actualizar
+			  M.toast({html: 'Actualizando...'});
+			  let producto = {
+					  	id:editando,
+						nombre,
+						precio,
+						cantidad
+					};
+			  
+				$.ajax({
+					method: "PUT",
+					url: "producto/update",
+					datatype: "JSON",
+					contentType: "application/json",
+					data: JSON.stringify(producto),
+					success: function(data, status, jqXHR ){
+						M.toast({html: '¡Actualizado!'});
+						editarTabla(data.id, data.nombre, data.precio, data.cantidad);
+					},
+					error: function(jqXHR , status, e){
+						M.toast({html: 'Error a crear un producto status: '+jqXHR.status});
+						console.log(jqXHR);
+					}
+				});
+		  }
+	  });
+});
+
+function editarTabla(id, nombre, precio, cantidad){
+	let markup = `<td>${nombre}</td><td>${precio}</td><td>${cantidad}</td><td><i id="btn${id}" class="small material-icons modal-trigger btn-editar" href="#modal1">edit</i> <i id="del${id}" class="del red-text small material-icons">delete</i></td>`;
+	$("tr[name='"+ id +"']").html(markup);
+}
+
+function agregarTabla(id, nombre, precio, cantidad){
+	let markup = `<tr name="${id}"><td>${nombre}</td><td>${precio}</td><td>${cantidad}</td><td><i id="btn${id}" class="small material-icons modal-trigger btn-editar" href="#modal1">edit</i> <i id="del${id}" class="del red-text small material-icons">delete</i></td></tr>`;
+	$('#tablaProductos tbody').append(markup);
+	
+	$("tr[name='"+ id +"']").on('click', '#btn'+id, function() {
+		editando = id;
+		let tds = $("tr[name='"+ id +"'] > td");
+		$('#nombreEdit').val(tds[0].innerHTML);
+		$('#precioEdit').val(tds[1].innerHTML);
+		$('#cantidadEdit').val(tds[2].innerHTML);
+		M.updateTextFields();
+	});
+	
+	$("tr[name='"+ id +"']").on('click', '#del'+id, function() {
+		// AJAX Request remove
+		M.toast({html: 'Eliminando...'});
+		$.ajax({
+			method: "DELETE",
+			url: "producto/delete",
+			data: JSON.stringify(id),
+			contentType: "application/json",
+			datatype: "JSON",
+			success: function(data, status, jqXHR ){
+				$("tr[name='"+ id +"']").remove();
+				M.toast({html: '¡Eliminado!'});
+			},
+			error: function(jqXHR , status, e){
+				M.toast({html: 'Error al eliminar status: '+jqXHR.status});
+				console.log(jqXHR);
+			}
+		});
+	});
+}
