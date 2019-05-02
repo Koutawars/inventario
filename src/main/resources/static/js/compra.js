38280cabd1f9094d1a3a6cbd606984cd4b5730e9
Koutawars <admikouta@gmail.com>
2019-05-02 10:55:43 -0500

    bug

--- src/main/resources/static/js/compra.js
+++ src/main/resources/static/js/compra.js
@@ -16,14 +16,16 @@ $(document).ready(function(){
 						listaTemp = data;
 						$('#tablaBuscar tbody').text("");
 						let enc = false;
+						let contador = 0;
 						for(let i = 0; i < data.length; i++){
 							enc = false;
 							carro.forEach(function(e){
 								if(e.id == data[i].id) enc = true;
 							});
 							if(enc) continue;
-							if(i < 3){
+							if(contador < 3){
 								agregarTablaBuscar(data[i]);
+								contador++;
 							}else{
 								break;
 							}
@@ -106,54 +108,60 @@ function agregarTablaBuscar(producto){
 	
 	$("tr[name='" + id + "']").on('click', '#add'+id, function() {
 		var cantidad = $('#cant' + id).val();
-		cants[id] = cantidad;
-		$("tr[name='"+ id +"']").remove();
-		carro.push(producto);
-		let markup = `
-			<tr name="${id}">
-				<td>${nombre}</td>
-				<td>${precio}</td>
-				<td>${cantidad} <i id="edit${id}" class="blue-text right small material-icons modal-trigger btn-del">edit</i></td>
-				<td><i id="del${id}" class="small material-icons modal-trigger btn-del">delete</i></td>
-			</tr>`;
-		$('#tablaDetalles tbody').append(markup);
-		$("tr[name='"+ id +"']").off('click', '#edit'+id);
-		$("tr[name='"+ id +"']").on('click', '#edit'+id, function() {
-			var td = $(this).parent()[0];
-			$(td).html("");
-			let mark = `
-			<div class="row">
-				<div class="col s8">
-					<input value="${cantidad}" placeholder="Cambiar cantidad" id="canti${id}" type="number" class="validate">
-				</div>
-				<div class="col s2">
-					<i id="save${id}" class="blue-text small material-icons modal-trigger btn-save">save</i>
-				</div>
-			</div>
-			`;
-			$(td).append(mark);
-			$("tr[name='"+ id +"']").off('click', '#save'+id);
-			$("tr[name='"+ id +"']").on('click', '#save'+id, function() {
-				console.log($("#canti" + id));
-				cantidad = $("#canti" + id).val();
-				cants[id] = cantidad;
+		if(cantidad != ""){
+			cants[id] = cantidad;
+			$("tr[name='"+ id +"']").remove();
+			carro.push(producto);
+			let markup = `
+				<tr name="${id}">
+					<td>${nombre}</td>
+					<td>${precio}</td>
+					<td>${cantidad} <i id="edit${id}" class="blue-text right small material-icons modal-trigger btn-del">edit</i></td>
+					<td><i id="del${id}" class="small material-icons modal-trigger btn-del">delete</i></td>
+				</tr>`;
+			$('#tablaDetalles tbody').append(markup);
+			$("tr[name='"+ id +"']").off('click', '#edit'+id);
+			$("tr[name='"+ id +"']").on('click', '#edit'+id, function() {
+				var td = $(this).parent()[0];
 				$(td).html("");
-				$(td).html(`${cants[id]} <i id="edit${id}" class="blue-text right small material-icons modal-trigger btn-del">edit</i>`);
+				let mark = `
+					<div class="row">
+					<div class="col s8">
+						<input value="${cantidad}" placeholder="Cambiar cantidad" id="canti${id}" type="number" class="validate">
+					</div>
+					<div class="col s2">
+						<i id="save${id}" class="blue-text small material-icons modal-trigger btn-save">save</i>
+					</div>
+					</div>
+					`;
+				$(td).append(mark);
+				$("tr[name='"+ id +"']").off('click', '#save'+id);
+				$("tr[name='"+ id +"']").on('click', '#save'+id, function() {
+					console.log($("#canti" + id));
+					cantidad = $("#canti" + id).val();
+					if(cantidad != ""){
+						cants[id] = cantidad;
+						$(td).html("");
+						$(td).html(`${cants[id]} <i id="edit${id}" class="blue-text right small material-icons modal-trigger btn-del">edit</i>`);
+						calcularTotal();
+					}else{
+						 M.toast({html: 'Elija una cantidad'});
+					}
+				});
+			});
+		
+			$("tr[name='"+ id +"']").on('click', '#del'+id, function() {
+				//remover
+				$("tr[name='"+ id +"']").remove();
+				carro.splice(carro.indexOf(producto), 1);
+				cants.splice(id, 1);
+				$( "#search" ).trigger( "keyup" );
 				calcularTotal();
 			});
-			
-			
-		});
-		
-		$("tr[name='"+ id +"']").on('click', '#del'+id, function() {
-			//remover
-			$("tr[name='"+ id +"']").remove();
-			carro.splice(carro.indexOf(producto), 1);
-			cants.splice(id, 1);
-			$( "#search" ).trigger( "keyup" );
 			calcularTotal();
-		});
-		calcularTotal();
+		}else{
+			 M.toast({html: 'Elija una cantidad'});
+		}
 	});
 }
 
